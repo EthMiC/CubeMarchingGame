e2bb0fea175601e5740a33d8c8be0ed4b6735a82
EthiopianMiddleChild <amannegesse@gmail.com>
2023-09-19 20:44:50 +0300

    added noise

--- ProceduralMesh.gd
+++ ProceduralMesh.gd
@@ -3,13 +3,17 @@ extends Node3D
 @export var GridSize:int;
 @export var VoxelSize:float;
 
+var noise = NoiseTexture3D.new();
+
 func _ready():
+	noise.noise = FastNoiseLite.new();
 	for x in GridSize:
 		for y in GridSize:
 			for z in GridSize:
 				var point = CSGBox3D.new();
 				point.translate(Vector3(x - GridSize / 2, y - GridSize / 2, z - GridSize / 2) * VoxelSize);
-				add_child(point);
+				if noise.noise.get_noise_3d(x, y, z) > 0.5:
+					add_child(point);
 
 func _process(delta):
 	pass
